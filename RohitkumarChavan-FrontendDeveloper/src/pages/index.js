edacf4100ca75582f40082493214d3c9f4716e25
Rohitkumar Chavan <109240176+chavanRk@users.noreply.github.com>
2023-03-02 14:51:33 +0530

    Initial commit
    React App for all SpaceX information.

---
+++ RohitkumarChavan-FrontendDeveloper/src/pages/index.js
@@ -0,0 +1,43 @@
+import Error from "./Error"
+import Homepage from "./Homepage"
+import Capsules from "./Capsules"
+import Cores from "./Cores"
+import Crew from "./Crew"
+import SingleCrew from "./SingleCrew"
+import Dragons from "./Dragons"
+import SingleDragon from "./SingleDragon"
+import Landpads from "./Landpads"
+import SingleLandPad from "./SingleLandPad"
+import SingleLaunch from "./SingleLaunch"
+import Launchpads from "./Launchpads"
+import SingleLaunchPad from "./SingleLaunchPad"
+import Payloads from "./Payloads"
+import Roadster from "./Roadster"
+import Rockets from "./Rockets"
+import SingleRocket from "./SingleRocket"
+import Ships from "./Ships"
+import SingleShip from "./SingleShip"
+import Starlink from "./Starlink"
+
+export {
+  Error,
+  Homepage,
+  Capsules,
+  Cores,
+  Crew,
+  SingleCrew,
+  Dragons,
+  SingleDragon,
+  Landpads,
+  SingleLandPad,
+  SingleLaunch,
+  Launchpads,
+  SingleLaunchPad,
+  Payloads,
+  Roadster,
+  Rockets,
+  SingleRocket,
+  Ships,
+  SingleShip,
+  Starlink,
+}
